2405a936770bb8b115c104a984d281d36f6c7d2c
KncskBenedek <koncsikbenedek@gmail.com>
2023-02-13 09:46:40 +0100

    alap

---
+++ src/model/Auto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author benedek
+ */
+public class Auto {
+    private boolean defekt;
+    
+    public void kereketCserel(){
+        
+    }
+    public boolean halad(){
+        return true;
+    }
+}
